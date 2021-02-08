b3b7fa0f7cb7733552508070675336362ceedef7
NobuhleBu <64545496+NobuhleBu@users.noreply.github.com>
2021-02-08 11:24:20 +0200

    Add files via upload

---
+++ IncidentApplication/Data/AdditionalUserClaimsPrincipalFactory.cs
@@ -0,0 +1,50 @@
+﻿using IdentityModel;
+using IncidentApplication.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IncidentApplication.Data
+{
+    public class AdditionalUserClaimsPrincipalFactory
+        : UserClaimsPrincipalFactory<ApplicationUser, IdentityRole>
+    {
+        public AdditionalUserClaimsPrincipalFactory(
+            UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager, 
+            IOptions<IdentityOptions> options) 
+            : base(userManager, roleManager, options)
+        {
+        }
+
+        public override async Task<ClaimsPrincipal> CreateAsync(ApplicationUser user)
+        {
+            var principal = await base.CreateAsync(user);
+            var identity = (ClaimsIdentity)principal.Identity;
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtClaimTypes.Role, user.Role)
+            };
+            identity.AddClaims(claims);
+            return principal;
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
+        {
+            var principal = await base.CreateAsync(user);
+            var identity = (ClaimsIdentity)principal.Identity;
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtClaimTypes.Role, user.Role)
+            };
+            identity.AddClaims(claims);
+            return await base.GenerateClaimsAsync(user);
+        }
+    }
+}
---
+++ IncidentApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using IncidentApplication.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncidentApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<IncidentApplication.Models.User> User { get; set; }
+        public DbSet<IncidentApplication.Models.UserRoles> User_Roles { get; set; }
+        public DbSet<IncidentApplication.Models.Incidents> Incidents { get; set; }
+        public DbSet<IncidentApplication.Models.IncidentStatus> IncidentStatus { get; set; }
+        public DbSet<IncidentApplication.Models.Task_Detail> TaskDetail { get; set; }
+        public DbSet<IncidentApplication.Models.RefreshToken> RefreshTokens { get; set; }
+
+        internal Task<IdentityResult> ChangePasswordAsync(string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
